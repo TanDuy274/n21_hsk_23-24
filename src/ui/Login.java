191b1875dfe732f17f87cfede5f5b902a8302f22
Chau <22662231.chau@student.iuh.edu.vn>
2024-04-21 22:32:06 +0700

    Change login

--- src/ui/Login.java
+++ src/ui/Login.java
@@ -1,108 +1,113 @@
 package ui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
+public class Login extends JFrame implements ActionListener {
 
-public class Login extends JFrame implements ActionListener{
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+    private JLabel imageLabel;
+    private JLabel lblName;
+    private JTextField txtName;
+    private JLabel lblPass;
+    private JTextField txtPass;
+    private JButton loginBtn;
+    private JPanel pnLogin;
+	private JLabel lblTiTle;
+	private JPanel pnTitle;
+	private Font Font;
+	private JPanel pnName;
+	private JPanel pnPass;
+	private JPanel pnBtn;
+	private JPanel pnName1;
+	private JPanel pnName2;
+	private JPanel pnPass1;
+	private JPanel pnPass2;
 
-	private JPanel pN;
-	private JPanel pC;
-	private JPanel pS;
-	private JLabel lblTitel;
-	private JLabel lblName;
-	private JTextField txtName;
-	private JLabel lblPass;
-	private JTextField txtPass;
-	private JButton loginBtn;
-	private JPanel pnLogin;
+    public Login() {
+        setTitle("Đăng nhập");
+        setSize(600, 300);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	public Login() {
-		// TODO Auto-generated constructor stub
-		setTitle("Đăng nhập");
-		setSize(350, 280);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+        leftPanel = new JPanel();
+        rightPanel = new JPanel();
+        JPanel phim1 = new JPanel();
+        ImageIcon img7 = new ImageIcon("img/logo4.jpg");
+		Image scaled7 = scaleImage(img7.getImage(), 320, 300);
+		ImageIcon imgScaled7 = new ImageIcon(scaled7);
+		JLabel hinh7 = new JLabel(imgScaled7);
+        leftPanel.add(hinh7);
+        leftPanel.setBackground(Color.white);
 		
-		pN = new JPanel();
-		pC = new JPanel();
-		pS = new JPanel();
-		lblTitel = new JLabel("Quản lý Vé xem phim");
-		lblTitel.setFont(new Font(getName(), Font.BOLD, 20));
-		lblTitel.setForeground(Color.BLUE);
-		Box panelName = Box.createVerticalBox();
-		lblName = new JLabel("Tên đăng nhập:");
-		txtName = new JTextField(25);
-		txtName.setPreferredSize(new Dimension(WIDTH, 26));
-		Box panelPass = Box.createVerticalBox();
-		lblPass = new JLabel("Mật khẩu:");
-//		txtPass = new JTextField(25);
-		txtPass = new JPasswordField(25);
-		txtPass.setPreferredSize(new Dimension(WIDTH, 26));
-		loginBtn = new JButton("Đăng nhập");
-		loginBtn.setPreferredSize(new Dimension(200, 34));
-		
-		panelName.add(Box.createVerticalStrut(20));
-		panelName.add(lblName);
-		panelName.add(Box.createVerticalStrut(10));
-		panelName.add(txtName);
-		panelPass.add(lblPass);
-		panelPass.add(Box.createVerticalStrut(10));
-		panelPass.add(txtPass);
-		pnLogin = new JPanel(new FlowLayout(FlowLayout.CENTER, 10,10));
-		pnLogin.add(loginBtn);
-		
-		
-		pN.add(lblTitel);
-		pC.add(panelName);
-		pC.add(panelPass);
-		pS.add(pnLogin);
-		
-		add(pN, BorderLayout.NORTH);
-		add(pC, BorderLayout.CENTER);
-		add(pS, BorderLayout.SOUTH);
-		
-		
-		
-		loginBtn.addActionListener(this);
-		
+//		infor
+//        title
+        pnTitle = new JPanel(new FlowLayout(FlowLayout.CENTER, 10,10));
+        lblTiTle = new JLabel("ĐĂNG NHẬP");
+        Font = new Font("Arial", Font.BOLD, 20);
+        lblTiTle.setFont(Font);
+        Color orangeDark = new Color(255, 128, 0); // Tạo màu cam tùy chỉnh
+        lblTiTle.setForeground(orangeDark);
+        pnTitle.add(lblTiTle);
+        
+//        ten dang nhap
+        Box BoxInfor = Box.createVerticalBox();
+        lblName = new JLabel("Tên đăng nhập:");
+        txtName = new JTextField(20); //tùy chỉnh chiều rộng
+        txtName.setPreferredSize(new Dimension(25, 25)); 
+        pnName1 = new JPanel(new FlowLayout(FlowLayout.CENTER,0,5));
+        pnName1.add(lblName);
+        pnName2 = new JPanel(new FlowLayout(FlowLayout.CENTER,0,5));
+        pnName2.add(txtName);
+        
+//        matkhau
+        lblPass = new JLabel("Mật khẩu:");
+        txtPass = new JPasswordField(20); //tùy chỉnh chiều rộng
+        txtPass.setPreferredSize(new Dimension(25, 25)); 
+        pnPass1 = new JPanel(new FlowLayout(FlowLayout.CENTER,0,5));
+        pnPass1.add(lblPass);
+        pnPass2 = new JPanel(new FlowLayout(FlowLayout.CENTER,0,5));
+        pnPass2.add(txtPass);
+//        button
+        loginBtn = new JButton("Đăng nhập");
+        loginBtn.setForeground(Color.white);
+        loginBtn.setBackground(orangeDark);
+        loginBtn.setPreferredSize(new Dimension(200, 34));
+        pnBtn = new JPanel(new FlowLayout(FlowLayout.LEFT,0,20));
+        pnBtn.add(loginBtn);
+        
+        BoxInfor.add(pnName1);
+        BoxInfor.add(pnName2);
+        BoxInfor.add(pnPass1);
+        BoxInfor.add(pnPass2);
+   
+        rightPanel.add(pnTitle, BorderLayout.SOUTH);
+        rightPanel.add(BoxInfor, BorderLayout.CENTER);
+        rightPanel.add(pnBtn, BorderLayout.NORTH);
+
+       
+        add(leftPanel,BorderLayout.WEST);
+        add(rightPanel, BorderLayout.CENTER);
+      
+        
+        loginBtn.addActionListener(this);
+    }
+
+    private Image scaleImage(Image image, int w, int h) {
+    	Image scaled = image.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+        return scaled;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Login().setVisible(true);
-	}
+        new Login().setVisible(true);
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		String username = txtName.getText();
-		String password = txtPass.getText();
-		if (username.equals("nhanvien1") && password.equals("NV123@")) {
-			JOptionPane.showMessageDialog(null, "Đăng nhập thành công!");
-			new QLPhim().setVisible(true);
-		}else if (username.isEmpty() || password.isEmpty()){
-			JOptionPane.showMessageDialog(null, "Không để trống!");
-		}else
-			JOptionPane.showMessageDialog(null, "Sai tên đăng nhập hoặc mật khẩu!");
-			setVisible(false);
-		
-	}
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Xử lý sự kiện đăng nhập
+    }
 }
